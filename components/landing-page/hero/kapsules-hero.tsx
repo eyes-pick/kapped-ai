1a217d99aeee2ddae0881760642e46ca6e0f09c8
eyes-pick <walshelachy@hotmail.com>
2025-06-13 12:13:54 +1000

    chore: remove client directive from kapsules hero

--- components/landing-page/hero/kapsules-hero.tsx
+++ components/landing-page/hero/kapsules-hero.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import LandingShell from "@components/landing-page/shell/LandingShell";
 import SearchForm from "@components/landing-page/prompt/SearchForm";
 import NavRow from "@components/landing-page/navRow/NavRow";
