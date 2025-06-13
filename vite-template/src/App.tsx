3b9357b3ae82f5dca0d9a95e580b2737cb45f18f
eyes-pick <walshelachy@hotmail.com>
2025-06-13 12:41:51 +1000

    add shadcn button and tailwind setup to vite template

---
+++ vite-template/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button";
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Hello Vite Template</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
